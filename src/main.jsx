8b4c8ec41494d9860b8dd1e522e9ce1d65b4dbcf
Gaurav9128 <jaingaurav@0212000@gmail.com>
2024-07-17 09:06:41 +0530

    New Changes Occur

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,7 @@ import VerificationPending from './pages/VerificationPending.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import PrivacyPolicy from './pages/PrivacyPolicy.jsx';
 import TermsConditions from './pages/TermsCondition.jsx';
+import AboutUs from './pages/AboutUs.jsx';
 
 
 
@@ -61,6 +62,10 @@ const router = createBrowserRouter([
   {
     path : "/terms-condition" ,
     element : <TermsConditions />
+  },
+  {
+    path : "/privacy-policy" ,
+    element : <AboutUs />
   }
 ]);
 
