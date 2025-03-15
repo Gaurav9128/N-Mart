2d6b7d431f7c428824b323e6d1d5950af379c131
Gaurav Jain <jaingaurav212000@gmail.com>
2025-03-15 22:41:21 +0530

    some message

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -63,7 +63,7 @@ const CartTotal = ({ cartItems, onCheckout }) => {
 
              <button
                 className="mt-6 w-full rounded-md bg-blue-500 py-2 font-medium text-white hover:bg-blue-400"
-                disabled={cartTotal < 5000}
+                // disabled={cartTotal < 5000}
                 onClick={onCheckout}
             >
                 PROCEED TO CHECKOUT
