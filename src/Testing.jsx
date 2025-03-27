cef3f746d115a07e69fb26e7bac99ad9f1f78264
Gaurav Jain <jaingaurav212000@gmail.com>
2025-03-27 09:27:09 +0530

    Testing file

--- src/Testing.jsx
+++ src/Testing.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { collection, query, where, getDocs, doc, updateDoc } from "firebase/firestore";
 import { firestore } from "./firebase/FirebaseConfig";
 
 const PaymentStatus = () => {
   const location = useLocation();
-  const navigate = useNavigate(); // Navigate function for redirection
   const [allData, setAllData] = useState(null);
 
   // Function to parse query parameters
@@ -70,7 +69,7 @@ const PaymentStatus = () => {
       // Redirect to home page if payment is successful or cancelled
       if (orderStatus.orderStatus === "success" || orderStatus.orderStatus === "failed") {
         setTimeout(() => {
-          navigate("https://www.n-mart.in/"); // Redirect to home page
+          window.location.href = "https://www.n-mart.in/"; // Redirect to home page
         }, 2000); // Wait for 2 seconds before redirecting
       }
     };
@@ -80,6 +79,7 @@ const PaymentStatus = () => {
   return (
     <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
       <h2>Thank You So much</h2>
+      <p>Redirecting you to N-Mart homepage...</p>
       <table border="1" cellPadding="10" style={{ width: "100%", borderCollapse: "collapse" }}>
         <thead>
           <tr>
