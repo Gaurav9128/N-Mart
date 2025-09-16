5872ca27f8b49e91f599cdd5deb5db22774daae7
Gaurav Jain <139974825+Gaurav9128@users.noreply.github.com>
2025-09-16 18:46:10 +0530

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -186,6 +186,7 @@ const decreaseQuantity = () => {
                         });
                     });
                     alert("Your product is updated in the cart!");
+                     fetchCartItems();
                 } else {
                     await addDoc(itemsCollection, {
                         productId: props.id,
@@ -198,6 +199,7 @@ const decreaseQuantity = () => {
                         productBrand: props.brand
                     });
                     alert("Your product is updated in the cart!");
+                     fetchCartItems();
                 }
     
                 getCartTotal();
